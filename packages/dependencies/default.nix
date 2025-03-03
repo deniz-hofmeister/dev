bf023ab07bdce1d0260246b2ca1f3d08ee8f712d
deniz hofmeister <deniz.hofmeister@gmail.com>
2025-03-03 11:37:34 +0100

    typescript LSP / treesitter

--- packages/dependencies/default.nix
+++ packages/dependencies/default.nix
@@ -4,14 +4,18 @@ let
   # Setup shell hook to configure paths for pkg-config
   opensslEnv = pkgs.symlinkJoin {
     name = "openssl-with-paths";
-    paths = [ openssl openssl.dev openssl.out ];
+    paths = [
+      openssl
+      openssl.dev
+      openssl.out
+    ];
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/openssl \
         --set PKG_CONFIG_PATH "${openssl.dev}/lib/pkgconfig"
     '';
   };
-  
+
   # Create shell hook to set OpenSSL environment variables
   opensslHook = ''
     export OPENSSL_ROOT_DIR=${openssl.dev}
@@ -21,44 +25,47 @@ let
   '';
 
   packages = [
-  binutils
-  black
-  cargo-nextest
-  ccls
-  clang-tools
-  cmake
-  curl
-  fzf
-  gcc
-  glibc
-  gnumake
-  isort
-  lldb
-  nixfmt-rfc-style
-  opensslEnv
-  # Explicit paths to support cmake find_package
-  pkg-config
-  podman-compose
-  prettierd
-  pyright
-  python313
-  python313Packages.debugpy
-  ripgrep
-  rustup
-  spdlog
-  stow
-  stylua
-  tmux
-  tmuxPlugins.sensible
-  tmuxPlugins.catppuccin
-  xclip
-  xdotool
-  xorg.xhost
-  xsel
-  zsh
-  nodejs
-  nodePackages.svelte-language-server
-];
+    binutils
+    black
+    cargo-nextest
+    ccls
+    clang-tools
+    cmake
+    curl
+    fzf
+    gcc
+    glibc
+    gnumake
+    isort
+    lldb
+    nixfmt-rfc-style
+    opensslEnv
+    # Explicit paths to support cmake find_package
+    pkg-config
+    podman-compose
+    prettierd
+    pyright
+    python313
+    python313Packages.debugpy
+    ripgrep
+    rustup
+    spdlog
+    stow
+    stylua
+    tmux
+    tmuxPlugins.sensible
+    tmuxPlugins.catppuccin
+    xclip
+    xdotool
+    xorg.xhost
+    xsel
+    zsh
+    nodejs
+    nodePackages.svelte-language-server
+    nodePackages.typescript-language-server
+    nodePackages.vscode-langservers-extracted # html, css, json, eslint
+    nodePackages.typescript
+  ];
 in
 {
   inherit packages;
--- packages/neovim/config/lua/lsp.lua
+++ packages/neovim/config/lua/lsp.lua
@@ -4,6 +4,11 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 lspconfig.clangd.setup({ capabilities = capabilities })
 lspconfig.pyright.setup({ capabilities = capabilities })
 lspconfig.svelte.setup({ capabilities = capabilities })
+lspconfig.tsserver.setup({ capabilities = capabilities })
+lspconfig.html.setup({ capabilities = capabilities })
+lspconfig.cssls.setup({ capabilities = capabilities })
+lspconfig.jsonls.setup({ capabilities = capabilities })
+lspconfig.eslint.setup({ capabilities = capabilities })
 
 local configs = require("lspconfig.configs")
 
@@ -40,3 +45,4 @@ vim.diagnostic.config({
 })
 
 require("lsp_lines").setup()
+
